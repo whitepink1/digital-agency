54708f9deb18c1aa9d8a7d83a5909c4647a09847
whitepink1 <kiruhat9@gmail.com>
2025-08-07 16:48:44 +0300

    Colors and font added

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Barlow } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const barlow = Barlow({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  display: 'swap',
 });
 
 export const metadata = {
@@ -19,8 +15,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/icons/site-logo.svg"/>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={` ${barlow.className} antialiased`}
       >
         {children}
       </body>
